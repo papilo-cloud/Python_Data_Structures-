5636dda42b4043b92e1407b9a3bb4645572c5b2e
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-07-07 14:12:42 +0100

    linear search on sorted array

---
+++ Algos/searching/linear_search.py
@@ -0,0 +1,9 @@
+def bubble_sort(list, search_value):
+    for i in range(len(list)):
+        if list[i] == search_value:
+            return i 
+        elif list[i] > search_value:
+            break
+    return False
+
+print(bubble_sort([3, 17, 22, 75, 80, 202], 22))
