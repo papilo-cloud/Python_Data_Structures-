7587b300749998d170ae9fe899e10ba266910fe0
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-07-23 23:16:47 +0100

    fibonacci sequence

---
+++ Algos/fibonacci_sequence.py
@@ -0,0 +1,11 @@
+def fibonacci(num):
+    previous_fib = 0
+    current_fib = 1
+    index = 0
+    
+    while index <= num:
+        print(previous_fib, end=' ')
+        current_fib = current_fib + previous_fib
+        previous_fib = current_fib - previous_fib
+
+fibonacci(8)
