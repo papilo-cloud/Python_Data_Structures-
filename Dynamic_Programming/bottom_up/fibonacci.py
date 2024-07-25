8e7b25c91b3220487e0d62f85417261c6db8eac0
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-07-25 16:03:26 +0100

    fibonacci

---
+++ Dynamic_Programming/bottom_up/fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(num):
+    previous_fib = 0
+    current_fib = 1
+    index = 0
+    
+    while index <= num:
+        print(previous_fib, end=' ')
+        current_fib = current_fib + previous_fib
+        previous_fib = current_fib - previous_fib
+        
+        index += 1
+fibonacci(7)
