900b3f32f8157b29b8bd104617f1d3f68194e772
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-08-05 19:58:58 +0100

    queues

--- data_structures/queues/queues.py
+++ data_structures/queues/queues.py
@@ -1,18 +1,18 @@
 class Queue:
     def __init__(self):
         self.data_store = []
+        self.size = 0
     
     def enqueue(self, elem):
         self.data_store.append(elem)
+        self.size += 1
     
     def dequeue(self):
+        self.size -= 1
         return self.data_store.pop(0)
-        
-    def size(self):
-        return len(self.data_store)
     
     def is_empty(self):
-        if len(self.data_store) == 0:
+        if self.size == 0:
             return True
         else:
             return False
@@ -22,5 +22,20 @@ class Queue:
             return False
         else:
             return self.data_store[0]
+    def to_string(self):
+        to_str = ''
+        for i in self.data_store:
+            to_str += i + "\n"
+        return to_str
         
 queue = Queue()
+
+print(queue.size)
+queue.enqueue('apple')
+queue.enqueue('banana')
+queue.enqueue('mango')
+print(queue.size)
+print(queue.peek())
+print(queue.dequeue())
+print(queue.peek())
+print(queue.to_string())
