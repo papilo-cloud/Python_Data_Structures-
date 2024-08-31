5cddd01db10afae7eaf8aa67d53a089d984b7f61
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-08-31 15:33:32 +0100

    quicksort

--- algorithms/sorting/quicksort.py
+++ algorithms/sorting/quicksort.py
@@ -1,4 +1,11 @@
 class QuickSort:
+    # As the name suggests, Quicksort is one of the fastest sorting algorithms.
+
+    # The Quicksort algorithm takes an array of values, chooses one of the 
+    # values as the 'pivot' element, and moves the other values so that 
+    # lower values are on the left of the pivot element, and higher values
+    # are on the right of it.
+    
     def __init__(self, array) -> None:
         self.array = array
         
