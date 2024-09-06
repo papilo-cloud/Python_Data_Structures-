5479d1ce663a38b054478fc99e6b7c3c01893943
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-06 23:58:29 +0100

    heap data structure

--- data_structures/Heap/heap.py
+++ data_structures/Heap/heap.py
@@ -16,3 +16,8 @@ class Heap:
     def parent_index(self, index):
         return (index - 1) // 2
     
+    def insert(self, value):
+        self.data.append(value)
+        new_node_index = len(self.data) - 1 
+        
+        # The following loop executes the "trickle up" algorithms.
