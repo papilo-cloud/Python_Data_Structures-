46ae3018e00e116750464983a33fa380a636a40a
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-06 23:37:46 +0100

    heap data structure

---
+++ data_structures/Heap/heap.py
@@ -0,0 +1,18 @@
+class Heap:
+    def __init__(self) -> None:
+        self.data = []
+    
+    def root_node(self):
+        return self.data[0]
+    
+    def last_node(self):
+        return self.data[-1]
+    def left_child(self, index):
+        return (index * 2) + 1
+    
+    def right_child(self, index):
+        return (index * 2) + 2
+    
+    def parent_index(self, index):
+        return (index - 1) // 2
+    
