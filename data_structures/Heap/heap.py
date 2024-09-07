b552d605e36248898f5a26f359fb7fb24e210bb1
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-07 02:33:02 +0100

    heap deletion

--- data_structures/Heap/heap.py
+++ data_structures/Heap/heap.py
@@ -1,6 +1,7 @@
 class Heap:
     def __init__(self) -> None:
         self.data = []
+        self.count = 0
     
     def root_node(self):
         return self.data[0]
@@ -18,6 +19,7 @@ class Heap:
     
     def insert(self, value):
         self.data.append(value)
+        self.count += 1
         new_node_index = len(self.data) - 1 
         
         # The following loop executes the "trickle up" algorithms.
@@ -29,6 +31,29 @@ class Heap:
             self.data[new_node_index] = temp
             
             new_node_index = self.parent_index(new_node_index)
+    
+    def delete(self):
+        self.data[0] = self.data.pop()
+        self.count -= 1
+        trickle_node_index = 0
+        
+        self.trickle_down(trickle_node_index)
+    
+    def trickle_down(self, index):
+        largest = index
+        if self.left_child(index) < self.count and (
+            self.data[self.left_child(index)] >= self.data[index]):
+            largest = self.left_child(index)
+            
+        elif self.right_child(index) < self.count and (
+            self.data[self.right_child(index)] >= self.data[index]):
+            largest = self.right_child(index)
+        
+        if largest != index:
+            temp = self.data[index]
+            self.data[index] = self.data[largest]
+            self.data[largest] = temp
+            self.trickle_down(largest)
 
 hp = Heap()
 hp.insert(20)
