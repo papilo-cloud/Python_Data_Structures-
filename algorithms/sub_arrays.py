dc004adae2ab72205c636ee29e13465ef7a0bdef
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-22 23:37:09 +0100

    sub array

--- algorithms/sub_arrays.py
+++ algorithms/sub_arrays.py
@@ -7,4 +7,12 @@ def sub_array(arr, start, end):
         print(arr[start : end+1])
         return sub_array(arr, start+1, end)
 
+def sub_array1(arr):
+    res = []
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            res.append(arr[i: j+1])
+    return res
+    
+
 sub_array([1,2,3], 0, 0)
