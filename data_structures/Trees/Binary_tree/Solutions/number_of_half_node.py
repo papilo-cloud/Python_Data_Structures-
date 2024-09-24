0bf75675d787c65a5a519c099eaf9f30d7a45eb7
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-24 22:18:57 +0100

    find number of half node in binary tree

---
+++ data_structures/Trees/Binary_tree/Solutions/number_of_half_node.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def number_of_leaves(root):
+    if not root:
+        return
+    
+    q = []
+    q.append(root)
+    count = 0
+    
+    while q:
+        node = q.pop(0)
+        if (node.left and not node.right) or (node.right and not node.left):
+            count += 1
+        else:
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+    return count
