d5035eb6336a2ba4e0ff4f661f9a26d840165980
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-24 22:13:58 +0100

    prefix sum

--- algorithms/prefix_sum.py
+++ algorithms/prefix_sum.py
@@ -8,3 +8,24 @@ def prefix_sum(list):
     return prefix
 
 print(prefix_sum(arr))
+
+def longest_sub_arr(arr, k):
+    prefix = max_len = 0
+    mpp = {}
+    
+    for i in range(len(arr)):
+        prefix += arr[i]
+        if prefix == k:
+            max_len = max(max_len, i + 1)
+        x = prefix - k
+        if x in  mpp:
+            ln = i - mpp[x]
+            max_len = max(max_len, ln)
+        
+        if prefix not in mpp:
+            mpp[prefix] = 1
+    print(mpp)
+    print(max_len)
+x = [1,2,3]
+k = 3
+longest_sub_arr(x, k)
