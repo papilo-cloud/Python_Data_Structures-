5820bc590c58bfbf741547d83530eef9fcaea704
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-25 17:05:47 +0100

    shortest path algorithms for unweighted graph

--- data_structures/Graphs/graph.py
+++ data_structures/Graphs/graph.py
@@ -2,12 +2,15 @@ class Vertex:
     def __init__(self, value) -> None:
         self.value = value
         self.adjacent_vertices = []
+    
+    def add_adjacent_vertex_undirected(self, vertex):
+        self.adjacent_vertices.append(vertex)
         
-    def add_adjacent_vertex(self, vertex):
+    def add_adjacent_vertex_directed(self, vertex):
         if vertex in self.adjacent_vertices:
             return
         self.adjacent_vertices.append(vertex)
-        vertex.add_adjacent_vertex(self)
+        vertex.add_adjacent_vertex_directed(self)
     
     def dfs_traverse(self, vertex, visited={}):
         visited[vertex.value] = 1
@@ -36,3 +39,29 @@ class Vertex:
             if searched_value:
                 return searched_value
         return None
+    
+    def shortest_path(self, start, stop):
+        visited = {}
+        previous_table = {}
+
+        visited[start.value] = 1
+        q = []
+        q.append(start)
+        
+        while q:
+            current = q.pop(0)
+
+            for adjacent in current.adjacent_vertices:
+                if not visited.get(adjacent.value):
+                    visited[adjacent.value] = 1
+                    q.append(adjacent)
+                    
+                    previous_table[adjacent.value] = current.value
+        
+        path = []
+        current_path = stop.value
+        while current_path:
+            path.insert(0, current_path)
+            current_path = \
+                previous_table.get(current_path)
+        return path
