d2b29c10740346f4bf43f4d548753b3eb376025f
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-10-05 14:33:57 +0100

    Tries

--- data_structures/Tries/Trie.py
+++ data_structures/Tries/Trie.py
@@ -1,6 +1,7 @@
 class TrieNode:
     def __init__(self) -> None:
         self.children = {}
+        self.is_word = False
 
 class Trie:
     def __init__(self) -> None:
@@ -16,23 +17,29 @@ class Trie:
                 new_node = TrieNode()
                 current_node.children[char] = new_node
                 current_node = new_node
-        current_node.children['*'] = None
-        
-    def search(self, word):
-        current_node = self.root
-        
+        current_node.is_word = True
+    
+    def find(self, word):
+        node = self.root
         for char in word:
-            if current_node.children.get(char):
-                current_node = current_node.children[char]
+            if node.children.get(char):
+                node = node.children[char]
             else:
                 return None
-        return current_node
+        return node
+        
+    def search(self, word):
+        node = self.find(word)
+        return node and node.is_word
+    
+    def is_prefic(self, prefix):
+        return self.find(prefix)
     
     def collect_all_words(self, node=None, word='', words=[]):
         current_node = node or self.root
         
         for (key, collection_node) in current_node.children.items():
-            if key == '*':
+            if key == True:
                 words.append(word)
             else:
                 self.collect_all_words(collection_node, word+key, words)
