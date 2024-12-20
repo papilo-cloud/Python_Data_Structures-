cfd332ab81da01a29bf5316c56d99d990596b139
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-20 21:16:50 +0100

    string reversal

--- Recursion/string_reversal.py
+++ Recursion/string_reversal.py
@@ -1,8 +1,16 @@
+
+string = 'hello'
+
+# Botton-Up approach
 def string_reversal(string, index):
     
     if index == 0:
         return string[0]
     return string[index] + string_reversal(string, index - 1)
     
-string = 'hello'
-print(string_reversal(string, len(string) - 1))
+
+# Top-Down approach
+def reversal(string):
+    if len(string) == 1:
+        return string[0]
+    return reversal(string[1:]) + string[0]
