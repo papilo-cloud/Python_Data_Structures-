e5017b693d8ee30498789e1ec8538a5bd9cce0e5
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-20 17:35:31 +0100

    sum of array

--- Recursion/sum_of_array.py
+++ Recursion/sum_of_array.py
@@ -1,6 +1,6 @@
-def sum(array, index = 1):
-    if len(array) == index:
-        return array[0]
-    return array[index] + sum(array, index + 1)
+def sum(array):
+    if not array:
+        return 0
+    return array[0] + sum(array[1:])
 
 print(sum([1, 2, 3, 4, 5]))
