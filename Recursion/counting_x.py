367cf6288f35619b592732868516ceddc2abe8f4
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-20 21:16:19 +0100

    counting x

--- Recursion/counting_x.py
+++ Recursion/counting_x.py
@@ -1,3 +1,6 @@
+string = 'xhexlxlxo'
+
+# Botton-Up approach
 def count_x(string, index = 0):
     if index == len(string):
         return 0
@@ -6,4 +9,13 @@ def count_x(string, index = 0):
         return 1 + count_x(string, index + 1)
     else:
         return count_x(string, index + 1)
-print(count_x('xhexlxlxo'))
+
+
+# Top-Down approach
+def count_x_2(string):
+    if not string:
+        return 0
+    if string[0] == 'x':
+        return 1 + count_x_2(string[1:])
+    else:
+        return 1 + count_x_2(string[1:])
